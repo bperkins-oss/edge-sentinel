3673c348d9da0ad733757a2da35f3e0a3574b020
Bill Perkins <bperkins@synmax.com>
2026-04-13 16:19:32 -0500

    Bump to v2.0.82

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.bp22intel.edgesentinel"
         minSdk = 26
         targetSdk = 35
-        versionCode = 81
-        versionName = "2.0.81"
+        versionCode = 82
+        versionName = "2.0.82"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
